006685b6694c02b00cd5796f0ad6d1def0d4ec83
CPardi <chris_pardi@yahoo.co.uk>
2016-04-09 13:19:51 +0100

    Commented experimental code.

--- Jurassic/Core/ScriptCompiler.cs
+++ Jurassic/Core/ScriptCompiler.cs
@@ -49,38 +49,38 @@ namespace Jurassic
                 _engine.Execute(code);
             }
 
-            var userType = module.DefineType(assemblyName, TypeAttributes.Public);
-            var restoreMethod = userType.DefineMethod("RestoreScriptEngine", MethodAttributes.Public | MethodAttributes.Static, typeof(ScriptEngine), new Type[] { });
-            var restoreMethodBody = restoreMethod.GetILGenerator();
+            //var userType = module.DefineType(assemblyName, TypeAttributes.Public);
+            //var restoreMethod = userType.DefineMethod("RestoreScriptEngine", MethodAttributes.Public | MethodAttributes.Static, typeof(ScriptEngine), new Type[] { });
+            //var restoreMethodBody = restoreMethod.GetILGenerator();
 
-            // Argument : ScriptEngine
-            restoreMethodBody.Emit(OpCodes.Newobj, typeof(ScriptEngine).GetConstructor(new Type[] { }));
+            //// Argument : ScriptEngine
+            //restoreMethodBody.Emit(OpCodes.Newobj, typeof(ScriptEngine).GetConstructor(new Type[] { }));
 
-            // Argument : ObjectScope
-            restoreMethodBody.Emit(OpCodes.Ldnull);
-            restoreMethodBody.Emit(OpCodes.Ldnull);
-            restoreMethodBody.EmitCall(OpCodes.Callvirt, typeof(ScriptEngine).GetMethod("get_Global"), new Type[] { });
-            restoreMethodBody.Emit(OpCodes.Ldc_I4_0);
-            restoreMethodBody.Emit(OpCodes.Ldc_I4_0);
-            restoreMethodBody.EmitCall(OpCodes.Call, typeof(ObjectScope).GetMethod("CreateRuntimeScope"), new Type[] { });
+            //// Argument : ObjectScope
+            //restoreMethodBody.Emit(OpCodes.Ldnull);
+            //restoreMethodBody.Emit(OpCodes.Ldnull);
+            //restoreMethodBody.EmitCall(OpCodes.Callvirt, typeof(ScriptEngine).GetMethod("get_Global"), new Type[] { });
+            //restoreMethodBody.Emit(OpCodes.Ldc_I4_0);
+            //restoreMethodBody.Emit(OpCodes.Ldc_I4_0);
+            //restoreMethodBody.EmitCall(OpCodes.Call, typeof(ObjectScope).GetMethod("CreateRuntimeScope"), new Type[] { });
 
-            // Argument: obj
-            restoreMethodBody.EmitCall(OpCodes.Callvirt, typeof(ScriptEngine).GetMethod("get_Global"), new Type[] { });
+            //// Argument: obj
+            //restoreMethodBody.EmitCall(OpCodes.Callvirt, typeof(ScriptEngine).GetMethod("get_Global"), new Type[] { });
 
-            restoreMethodBody.EmitCall(OpCodes.Call, module.GetType("JavaScriptClass0").GetMethod("global_"), new Type[] { });
-            restoreMethodBody.Emit(OpCodes.Ret);
-            userType.CreateType();
+            //restoreMethodBody.EmitCall(OpCodes.Call, module.GetType("JavaScriptClass0").GetMethod("global_"), new Type[] { });
+            //restoreMethodBody.Emit(OpCodes.Ret);
+            //userType.CreateType();
 
             assemblyBuilder.Save(Path.GetFileName(dllPath));
         }
 
-        public ScriptEngine Load(Func<ScriptEngine, Scope, object, object>[] globals, FunctionDelegate[] testRef)
+        public ScriptEngine Load(Func<ScriptEngine, Scope, object, object>[] globals, FunctionDelegate[] anonymousDelegates)
         {
             var engine = new ScriptEngine();
             GeneratedMethod.generatedMethodCache = new Dictionary<long, WeakReference>();
 
             GeneratedMethod.generatedMethodID = 0;
-            foreach (var func in testRef)
+            foreach (var func in anonymousDelegates)
             {
                 GeneratedMethod.generatedMethodCache.Add(GeneratedMethod.generatedMethodID, new WeakReference(new GeneratedMethod(func, null)));
                 GeneratedMethod.generatedMethodID++;
